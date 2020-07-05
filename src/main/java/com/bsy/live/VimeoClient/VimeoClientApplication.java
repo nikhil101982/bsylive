59fbcf4e4dbc479aca8f62913aff6772940fd8a2
NikhilAgrawal <nikhil.agrl@gmail.com>
2020-07-05 11:01:37 +0530

    Adding client project

---
+++ src/main/java/com/bsy/live/VimeoClient/VimeoClientApplication.java
@@ -0,0 +1,13 @@
+package com.bsy.live.VimeoClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VimeoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VimeoClientApplication.class, args);
+	}
+
+}
