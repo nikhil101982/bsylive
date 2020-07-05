de62941560ceaaaa0c4148bc350ee520699dc670
NikhilAgrawal <nikhil.agrl@gmail.com>
2020-07-05 12:12:48 +0530

    Vimeo client project

---
+++ Vimeo/src/test/java/com/bsy/live/VimeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsy.live;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VimeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
